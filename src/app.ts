3720c80b0b138daecfdcd07d96c019c496c21706
jasmin <cjm98@smartel.co.kr>
2025-11-26 14:21:06 +0900

    swagger 추가

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,45 @@
 import { FastifyPluginAsync } from "fastify";
 import AutoLoad from "@fastify/autoload";
 import { fileURLToPath } from "url";
+import cors from "@fastify/cors";
 import { dirname, join } from "path";
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUi from "@fastify/swagger-ui";
 import databasePlugin from "./plugins/database.js";
+import {swaggerOptions, swaggerUiOptions} from "./lib/swagger.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app: FastifyPluginAsync = async (fastify, opts) => {
-
+    await fastify.register(cors, {
+        origin: true,
+        // origin: ["http://localhost:3000", "http://127.0.0.1:3000"], // 이런 식으로 제한도 가능
+        credentials: true,
+    });
+    await fastify.register(fastifySwagger, swaggerOptions)
+    await fastify.register(fastifySwaggerUi, swaggerUiOptions)
     await fastify.register(databasePlugin);
+
+    fastify.register((app, options, done) => {
+        app.get("/", {
+            schema: {
+                tags: ["Default"],
+                response: {
+                    200: {
+                        type: "object",
+                        properties: {
+                            anything: { type: "string" },
+                        },
+                    },
+                },
+            },
+            handler: (req, res) => {
+                res.send({ anything: "meaningfull" });
+            },
+        });
+        done();
+    });
 
     fastify.register(AutoLoad, {
         dir: join(__dirname, "routes"),
