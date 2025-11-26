7e35ba25cdaf6111e2f01fbd1b90b15365f1c2b6
jasmin <cjm98@smartel.co.kr>
2025-11-26 16:10:38 +0900

    refactor : workspace get by id 관계데이터들 (member, project) 도 조회가능하도록 수정

--- src/db/schema/project.ts
+++ src/db/schema/project.ts
@@ -5,8 +5,8 @@ import {
     timestamp,
 } from "drizzle-orm/pg-core";
 import {relations} from "drizzle-orm";
-import {workspaces} from "./workSpace";
-import {board} from "./board";
+import {workspaces} from "./workSpace.js";
+import {board} from "./board.js";
 
 export const project = pgTable("project", {
     id: uuid("id").defaultRandom().primaryKey(),
